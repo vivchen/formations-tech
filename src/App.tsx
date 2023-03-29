80f32134bfa75ad5cf7863a37475448f87838a4b
Vivian Chen <vivianchen@DavidsMookM1Pro.lan>
2023-03-29 09:09:58 -0700

    import useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { SampleTable} from "./components/SampleTable";
 
 function App() {
+
+  useEffect(() => {
+    fetch('/transactions', {
+      headers: {
+        Accept: 'application/json'
+      }
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+    }, []);
+
   return (
       <div className="App">
        <header className="App-header">
